57a7daec37882d86379e205837cb938b19c78082
Prasanna Gurdev <prasannagurdev@192.168.1.10>
2025-02-02 18:32:57 +0530

    uploading to git

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Peer-Mentor Learning Series Workshop 1</h1>
+        <div className="user-info">
+          <p><strong>Name:</strong>  Name prasanna</p> 
+          <p><strong>USN:</strong>  USN 1234567</p>
+           <p><strong>Name:</strong>  Name rakshiths</p> 
+          <p><strong>USN:</strong>  USN 456789</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
